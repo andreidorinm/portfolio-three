44e7a7f60ce2c8cdc5031199cdf39f8f88d07657
andreidorinm <andrei.dorin09@yahoo.com>
2023-10-14 23:56:33 +0300

    init project with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { buttonVariants } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="flex flex-col items-center justify-center min-h-screen space-y-20">
+      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Vite, React, Tailwind minimal starter
+      </h1>
+      <a
+        href="https://github.com/moinulmoin/vite-react-tailwind-starter"
+        target="_blank"
+        rel="noreferrer"
+        className={buttonVariants()}
+      >
+        <span className="text-xl hover:underline underline-offset-2">Star on GitHub</span>&nbsp;:)
+      </a>
+    </main>
+  );
 }
 
-export default App
+export default App;
